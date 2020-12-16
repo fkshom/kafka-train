04ee304efc291d0174532467fe7cd6b13a4e3c42
Shoma FUKUDA <fkshom@gmail.com>
2020-12-16 12:09:56 +0900

    work ssl

--- createcert.sh
+++ createcert.sh
@@ -16,13 +16,13 @@ PASSWORD=test1234
 VALIDITY=365
 
 # create CA
-openssl req -new -x509 -keyout ca-key -out ca-cert -days $VALIDITY -subj "/C=US/ST=California/L=San Diego/O=Development/OU=Dev/CN=example.com" -passout pass:$PASSWORD
+openssl req -new -x509 -keyout ca-key -out ca-cert -days $VALIDITY -subj "/C=US/ST=California/L=San Diego/O=Development/OU=Dev/CN=example.com" -passin pass:$PASSWORD -passout pass:$PASSWORD
 keytool -import -keystore kafka.server.truststore.jks -alias CARoot -file ca-cert -storepass $PASSWORD -keypass $PASSWORD -noprompt
 keytool -import -keystore kafka.client.truststore.jks -alias CARoot -file ca-cert -storepass $PASSWORD -keypass $PASSWORD -noprompt
 
 # Create server cert
-keytool -genkey -keystore kafka.server.keystore.jks -alias localhost -validity $VALIDITY \
-   -dname "CN=mqttserver.ibm.com, OU=ID, O=IBM, L=Hursley, S=Hants, C=GB" -storepass $PASSWORD -keypass $PASSWORD -noprompt
+keytool -genkey -keystore kafka.server.keystore.jks -alias localhost -validity $VALIDITY -keyalg RSA \
+   -dname "CN=localhost, OU=TEST, O=CONFLUENT, L=PaloAlto, S=Ca, C=US" -storepass $PASSWORD -keypass $PASSWORD -noprompt
 keytool -certreq -keystore kafka.server.keystore.jks -alias localhost -file cert-file -storepass $PASSWORD -keypass $PASSWORD -noprompt
 openssl x509 -req -CA ca-cert -CAkey ca-key -in cert-file -out cert-signed -days $VALIDITY -CAcreateserial -passin pass:$PASSWORD
 keytool -import -keystore kafka.server.keystore.jks -alias CARoot -file ca-cert -storepass $PASSWORD -keypass $PASSWORD -noprompt
@@ -30,10 +30,14 @@ keytool -import -keystore kafka.server.keystore.jks -alias localhost -file cert-
 rm cert-file cert-signed
 
 # Create client cert
-keytool -genkey -keystore kafka.client.keystore.jks -alias localhost -validity $VALIDITY \
-   -dname "CN=mqttserver.ibm.com, OU=ID, O=IBM, L=Hursley, S=Hants, C=GB" -storepass $PASSWORD -keypass $PASSWORD -noprompt
+keytool -genkey -keystore kafka.client.keystore.jks -alias localhost -validity $VALIDITY -keyalg RSA \
+   -dname "CN=localhost, OU=TEST, O=CONFLUENT, L=PaloAlto, S=Ca, C=US" -storepass $PASSWORD -keypass $PASSWORD -noprompt
 keytool -certreq -keystore kafka.client.keystore.jks -alias localhost -file cert-file -storepass $PASSWORD -keypass $PASSWORD -noprompt
 openssl x509 -req -CA ca-cert -CAkey ca-key -in cert-file -out cert-signed -days $VALIDITY -CAcreateserial -passin pass:$PASSWORD
 keytool -import -keystore kafka.client.keystore.jks -alias CARoot -file ca-cert -storepass $PASSWORD -keypass $PASSWORD -noprompt
 keytool -import -keystore kafka.client.keystore.jks -alias localhost -file cert-signed -storepass $PASSWORD -keypass $PASSWORD -noprompt
 rm cert-file cert-signed
+
+echo "$PASSWORD" > kafka_sslkey_creds
+echo "$PASSWORD" > kafka_keystore_creds
+echo "$PASSWORD" > kafka_truststore_creds
